1cd4ff094f033e500522cf4055caf9bedfc947de
Javier Enrique Ramirez Baquer <JERamirez@marpico.com.co>
2024-04-16 16:02:51 -0500

    veridicaion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//modulos
+import { ReactiveFormsModule}  from '@angular/forms';
+
+
+//componentes
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListProductsComponent } from './components/list-products/list-products.component';
 import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
@@ -16,7 +23,9 @@ import { AddEditProductComponent } from './components/add-edit-product/add-edit-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [
     provideClientHydration()
--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { product } from '../../interfaces/product';
+
 
 @Component({
   selector: 'app-add-edit-product',
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
-export class AddEditProductComponent {
+export class AddEditProductComponent implements OnInit {
+  form: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [null, Validators.required],
+      stock: [null, Validators.required],
+    })
+
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  addproduct() {
+    console.log(this.form.value.name);
+    console.log(this.form.get('name')?.value);
+
+    const product: product = {
+
+      name: this.form.value.name,
+      description: this.form.value.description,
+      price: this.form.value.price,
+      stock: this.form.value.stock
+
+    }
+
+  }
+
+
+
 
 }
+
+
+
+
+
+
